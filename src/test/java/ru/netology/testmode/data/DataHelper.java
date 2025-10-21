e208b6edf767d9338d117e8ea430f4a8d3683ae2
Alexey <erg163@ya.ru>
2025-10-21 16:39:51 +0400

    Finish Test

--- src/test/java/ru/netology/testmode/data/DataHelper.java
+++ src/test/java/ru/netology/testmode/data/DataHelper.java
@@ -2,31 +2,57 @@ package ru.netology.testmode.data;
 
 import lombok.Value;
 
+import java.util.Random;
+
 public class DataHelper {
 
     private DataHelper() {
-
     }
 
-    public static AuthInfo getAuthInfo() {
+    public static AuthInfo getAuthInfo() { //возвращает данные валидного пользователя
         return new AuthInfo("vasya", "qwerty123");
-
     }
 
-public static VertificationCode getVeritificationCode(AuthInfo authInfo) {
-    return new VertificationCode("12345");
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) { //данные другого пользователя
+        return new AuthInfo("petya", "123qwerty");
+    }
+
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) { //возвращает код верификации
+        return new VerificationCode("12345");
+    }
+
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static CardInfo getSecondCardInfo() {
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static int validAmount(int balance) {
+        return new Random().nextInt(Math.abs(balance)) + 1;
+    }
+
+    public static int invalidAmount(int balance) {
+        return Math.abs(balance) + new Random().nextInt(1000);
+    }
+
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    @Value
+    public static class CardInfo {
+        String cardNumber;
+        String cardId;
+    }
 }
 
-@Value
-public static class AuthInfo {
-    String login;
-    String password;
-}
-
-@Value
-public static class VertificationCode {
-    private String code;
-}
-
-
-}
--- src/test/java/ru/netology/testmode/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/testmode/test/MoneyTransferTest.java
@@ -1,4 +1,71 @@
 package ru.netology.testmode.test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.testmode.data.DataHelper;
+import ru.netology.testmode.page.DashBoardPage;
+import ru.netology.testmode.page.LoginPageV1;
+import ru.netology.testmode.page.LoginPageV2;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.testmode.data.DataHelper.getAuthInfo;
+
 public class MoneyTransferTest {
+    int firstCardBalanceBefore;
+    int secondCardBalanceBefore;
+    DashBoardPage dashBoardPage;
+    DataHelper.CardInfo firstCard;
+    DataHelper.CardInfo secondCard;
+
+
+    @BeforeEach
+
+    public void setup() {
+        var info = getAuthInfo();
+        var verificationCode = DataHelper.getVerificationCode(info);
+        var loginPage = Selenide.open("http://localhost:9999", LoginPageV1.class);
+        var verificationPage = loginPage.validLogin(info);
+        dashBoardPage = verificationPage.validVerify(verificationCode);
+        firstCard = DataHelper.getFirstCardInfo(); //данные карты 1
+        secondCard = DataHelper.getSecondCardInfo();//данные карты 2
+        //Получаем баланс карт
+        firstCardBalanceBefore = dashBoardPage.getCardBalance(firstCard);
+        secondCardBalanceBefore = dashBoardPage.getCardBalance(secondCard);
+    }
+
+    @Test
+    public void shouldSuccessfulReplenishmentOfFirstCard() {          //Успешный перевод
+        var validAmount = DataHelper.validAmount(firstCardBalanceBefore);
+        var transferPage = dashBoardPage.selectCard(firstCard);
+        transferPage.validTransferMoney(validAmount, secondCard);
+
+        int firstCardBalanceAfter = dashBoardPage.getCardBalance(firstCard);
+        int secondCardBalanceAfter = dashBoardPage.getCardBalance(secondCard);
+
+        assertEquals(firstCardBalanceBefore + validAmount, firstCardBalanceAfter);
+        assertEquals(secondCardBalanceBefore - validAmount, secondCardBalanceAfter);
+    }
+
+
+    @Test
+    public void shouldUnsuccessfulCardToCardTransfer() {         //Перевод с ошибкой
+        var validAmount = DataHelper.invalidAmount(firstCardBalanceBefore);
+        var transferPage = dashBoardPage.selectCard(firstCard);
+        transferPage.transferMoney(validAmount, secondCard);
+
+        transferPage.shouldMessageError("Сумма списания превышает баланс карты.");
+
+        int firstCardBalanceAfter = dashBoardPage.getCardBalance(firstCard);
+        int secondCardBalanceAfter = dashBoardPage.getCardBalance(secondCard);
+
+        assertEquals(firstCardBalanceBefore , firstCardBalanceAfter);
+        assertEquals(secondCardBalanceBefore , secondCardBalanceAfter);
+    }
 }
+
+
+
+
